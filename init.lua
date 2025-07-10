39ff99431d671016af0ba30eb335cae79b892ec1
bedheadkitten <bedheadkitten@proton.me>
2025-07-10 22:23:43 +0200

    softer dracula

--- init.lua
+++ init.lua
@@ -830,10 +830,6 @@ require('lazy').setup({
   },
 
   { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     --    'folke/tokyonight.nvim',
     'Mofiqul/dracula.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -842,18 +838,26 @@ require('lazy').setup({
       require('dracula').setup {
         transparent_bg = true,
         italics_comment = false,
-
-        -- styles = {
-        --   comments = { italic = false }, -- Disable italics in comments
-        -- },
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'dracula' -- tokyonight-night
+      vim.cmd.colorscheme 'dracula-soft' -- tokyonight-night
     end,
   },
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     require('kanagawa').setup {
+  --       transparent = true,
+  --       commentStyle = { italic = true },
+  --       keywordStyle = { italic = true },
+  --     }
+  --     vim.cmd.colorscheme 'kanagawa-lotus'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
